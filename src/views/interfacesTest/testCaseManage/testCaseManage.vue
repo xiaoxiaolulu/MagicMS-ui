98436f3ae451a475f18d91af977307d0c4a3b053
Null <546464268@qq.com>
2019-12-18 13:58:32 +0800

    优化

--- src/views/interfacesTest/testCaseManage/testCaseManage.vue
+++ src/views/interfacesTest/testCaseManage/testCaseManage.vue
@@ -2,21 +2,8 @@
     <div>
         <div class="lk-toolbar">
             <el-form :inline="true" :model="queryParams" ref="queryParams">
-                <el-form-item label="接口名称"  prop="name">
+                <el-form-item label="用例名称"  prop="name">
                     <el-input size="small" clearable type="name" v-model.trim="queryParams.name" placeholder="接口名称"></el-input>
-                </el-form-item>
-                <el-form-item label="接口地址"  prop="router">
-                    <el-input size="small" clearable type="router" v-model.trim="queryParams.router" placeholder="接口地址"></el-input>
-                </el-form-item>
-                <el-form-item label="请求类型" prop="type">
-                    <el-select size="small" type="type" v-model="queryParams.type" v-loading="loading"
-                               element-loading-spinner="el-icon-loading" clearable filterable>
-                        <el-option v-for="item in type" :value="item" :key="null">
-                        </el-option>
-                    </el-select>
-                </el-form-item>
-                <el-form-item label="测试项目"  prop="project">
-                    <el-input size="small" clearable type="project" v-model.trim="queryParams.project" placeholder="测试项目"></el-input>
                 </el-form-item>
                 <el-form-item>
                     <el-button size="small" type="primary" icon="el-icon-search" @click="queryList"></el-button>
@@ -36,13 +23,9 @@
                 height="100%"
                 border>
             <el-table-column type="index" width="50" label="序号"></el-table-column>
-            <el-table-column prop="interface_name" min-width="150" label="接口名称"></el-table-column>
-            <el-table-column prop="url" min-width="150" label="接口地址"></el-table-column>
-            <el-table-column prop="method" min-width="150" label="接口方法"></el-table-column>
-            <el-table-column prop="headers" min-width="150" label="请求头部"></el-table-column>
-            <el-table-column prop="params" min-width="150" label="请求参数"></el-table-column>
-            <el-table-column prop="project.name" min-width="150" label="测试项目"></el-table-column>
-            <el-table-column prop="desc" min-width="150" label="接口描述"></el-table-column>
+            <el-table-column prop="test_name" min-width="150" label="用例名称"></el-table-column>
+            <el-table-column prop="desc" min-width="150" label="用例描述"></el-table-column>
+            <el-table-column prop="assertion" min-width="150" label="断言数据"></el-table-column>
             <el-table-column prop="creator.nick_name" min-width="150" label="创建人"></el-table-column>
             <el-table-column prop="add_time" min-width="150" label="创建时间"></el-table-column>
             <el-table-column label="操作" min-width="150">
@@ -67,7 +50,7 @@
 <script>
     import addDialog from './addDialog'
     import editDialog from "./editDialog";
-    import {getApiList,  deleteApi} from "../../../api/api";
+    import {getCaseList,  deleteApi} from "../../../api/api";
 
 
     export default {
@@ -75,12 +58,8 @@
             return {
                 queryParams: {
                     name: null,
-                    router: null,
-                    type: null,
-                    project: null,
                     currentPage: 1
                 },
-                type: ["GET", "POST", "DELETE", "PUT"],
                 name: null,
                 addDialogVisible: false,
                 editDialogVisible: false,
@@ -106,7 +85,7 @@
             },
             queryList() {
                 this.tableLoading = true;
-                getApiList({ params: this.queryParams }).then((response) => {
+                getCaseList({ params: this.queryParams }).then((response) => {
                     this.tableLoading = false;
                     console.log(response.data.data);
                     this.tableData = response.data.data;
